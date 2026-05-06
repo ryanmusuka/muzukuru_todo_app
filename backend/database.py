68a6510fb8ae0e50ea1f0ca8fbdd2d1989dcfb64
Ryan Musuka <ryanmusuka5@gmail.com>
2026-05-06 20:57:21 +0200

    correction in database

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,10 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todoapp.db"
+load_dotenv()
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, 
